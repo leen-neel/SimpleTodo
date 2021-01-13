45594405251de2e090650f5353e345f1a964f64f
AndyTheGameDev <and24903@gmail.com>
2021-01-13 14:34:13 +0530

    Changed header

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-12">
                 <p class="display-3">
-                    Vue crash course
+                    Todo List App
                 </p>
             </div>
         </div>
